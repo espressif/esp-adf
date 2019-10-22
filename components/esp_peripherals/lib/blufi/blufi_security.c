3a063092903275b4f9b3f11c9b49879415f67116
hanxiaodong <hanxiaodong@espressif.com>
2019-10-22 20:40:35 +0800

    fix adf bt demo cmake error Closes https://github.com/espressif/esp-adf/issues/243

--- components/esp_peripherals/lib/blufi/blufi_security.c
+++ components/esp_peripherals/lib/blufi/blufi_security.c
@@ -27,20 +27,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
-#include "esp_bt.h"
 #include "esp_log.h"
-
-#include "esp_blufi_api.h"
-#include "esp_bt_defs.h"
-#include "esp_gap_ble_api.h"
-#include "esp_bt_main.h"
+#include "esp_system.h"
 
 #include "mbedtls/aes.h"
 #include "mbedtls/dhm.h"
