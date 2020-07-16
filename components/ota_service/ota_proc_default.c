f915912c033797e0e87ed52aaba303a67c04850f
donglianghao <donglianghao@espressif.com>
2020-07-16 16:56:30 +0800

    Add firmware version check and compare in korvo1906 example

--- components/ota_service/ota_proc_default.c
+++ components/ota_service/ota_proc_default.c
@@ -109,7 +109,7 @@ static ota_service_err_reason_t ota_app_partition_prepare(void **handle, ota_nod
         esp_http_client_config_t config = {
             .url = node->uri,
             .cert_pem = node->cert_pem,
-            .timeout_ms = 1000,
+            .timeout_ms = 5000,
         };
         esp_https_ota_config_t ota_config = {
             .http_config = &config,
--- examples/korvo_du1906/main/app_ota_upgrade.c
+++ examples/korvo_du1906/main/app_ota_upgrade.c
@@ -29,9 +29,11 @@
 #include "freertos/event_groups.h"
 #include "esp_log.h"
 #include "audio_error.h"
+#include "audio_mem.h"
 #include "esp_partition.h"
 #include "ota_service.h"
 #include "ota_proc_default.h"
+#include "tone_stream.h"
 
 static const char *TAG = "APP_OTA_UPGRADE";
 
@@ -41,6 +43,7 @@ static EventGroupHandle_t OTA_FLAG;
 
 static ota_service_err_reason_t ota_sdcard_image_need_upgrade(void *handle, ota_node_attr_t *node)
 {
+    AUDIO_NULL_CHECK(TAG, handle, return OTA_SERV_ERR_REASON_NULL_POINTER);
     AUDIO_NULL_CHECK(TAG, node->uri, return OTA_SERV_ERR_REASON_NULL_POINTER);
     const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, node->label);
     if (partition == NULL) {
@@ -49,7 +52,6 @@ static ota_service_err_reason_t ota_sdcard_image_need_upgrade(void *handle, ota_
     }
 
     char *uri = strstr(node->uri, "/sdcard/");
-    AUDIO_NULL_CHECK(TAG, uri, return OTA_SERV_ERR_REASON_URL_PARSE_FAIL);
     if (access(uri, 0) == 0) {
         ESP_LOGI(TAG, "Found ota file in sdcard, uri: %s", uri);
         esp_err_t err = ESP_OK;
@@ -63,12 +65,95 @@ static ota_service_err_reason_t ota_sdcard_image_need_upgrade(void *handle, ota_
     }
 }
 
+static ota_service_err_reason_t ota_http_tone_image_need_upgrade(void *handle, ota_node_attr_t *node)
+{
+    bool                need_write_desc = false;
+    flash_tone_header_t cur_header      = { 0 };
+    flash_tone_header_t incoming_header = { 0 };
+    esp_app_desc_t      current_desc    = { 0 };
+    esp_app_desc_t      incoming_desc   = { 0 };
+    esp_err_t           err             = ESP_OK;
+
+    /* try to get the tone partition*/
+    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_ANY, node->label);
+    if (partition == NULL) {
+        ESP_LOGE(TAG, "data partition [%s] not found", node->label);
+        return OTA_SERV_ERR_REASON_PARTITION_NOT_FOUND;
+    }
+
+    if (tone_partition_verify() == ESP_FAIL) {
+        esp_partition_erase_range(partition, 0, partition->size);
+        return OTA_SERV_ERR_REASON_SUCCESS;
+    }
+
+    /* Read tone header from partition */
+    if (esp_partition_read(partition, 0, (char *)&cur_header, sizeof(flash_tone_header_t)) != ESP_OK) {
+        return OTA_SERV_ERR_REASON_PARTITION_RD_FAIL;
+    }
+
+    /* Read tone header from incoming stream */
+    if (ota_data_image_stream_read(handle, (char *)&incoming_header, sizeof(flash_tone_header_t)) != ESP_OK) {
+        return OTA_SERV_ERR_REASON_STREAM_RD_FAIL;
+    }
+    if (incoming_header.header_tag != 0x2053) {
+        ESP_LOGE(TAG, "not audio tone bin");
+        return OTA_SERV_ERR_REASON_UNKNOWN;
+    }
+    ESP_LOGI(TAG, "format %d : %d", cur_header.format, incoming_header.format);
+    /* upgrade the tone bin when incoming bin format is 0 */
+    if (incoming_header.format == 0) {
+        goto write_flash;
+    }
+
+    /* read the app desc from incoming stream when bin format is 1*/
+    if (ota_data_image_stream_read(handle, (char *)&incoming_desc, sizeof(esp_app_desc_t)) != ESP_OK) {
+        return OTA_SERV_ERR_REASON_STREAM_RD_FAIL;
+    }
+    ESP_LOGI(TAG, "imcoming magic_word %X, project_name %s", incoming_desc.magic_word, incoming_desc.project_name);
+    /* check the incoming app desc */
+    if (incoming_desc.magic_word != FLASH_TONE_MAGIC_WORD) {
+        return OTA_SERV_ERR_REASON_ERROR_MAGIC_WORD;
+    }
+    if (strstr(FLASH_TONE_PROJECT_NAME, incoming_desc.project_name) == NULL) {
+        return OTA_SERV_ERR_REASON_ERROR_PROJECT_NAME;
+    }
+
+    /* compare current app desc with the incoming one if the current bin's format is 1*/
+    if (cur_header.format == 1) {
+        if (tone_partition_get_app_desc(&current_desc) != ESP_OK) {
+            return OTA_SERV_ERR_REASON_PARTITION_RD_FAIL;
+        }
+        if (ota_get_version_number(incoming_desc.version) < 0) {
+            return OTA_SERV_ERR_REASON_ERROR_VERSION;
+        }
+        ESP_LOGI(TAG, "current version %s, incoming version %s", current_desc.version, incoming_desc.version);
+        if (ota_get_version_number(incoming_desc.version) <= ota_get_version_number(current_desc.version)) {
+            ESP_LOGW(TAG, "The incoming version is same as or lower than the running version");
+            return OTA_SERV_ERR_REASON_NO_HIGHER_VERSION;
+        }
+    }
+
+    /* incoming bin's format is 1, and the current bin's format is 0, upgrade the incoming one */
+    need_write_desc = true;
+
+write_flash:
+    if ((err = esp_partition_erase_range(partition, 0, partition->size)) != ESP_OK) {
+        ESP_LOGE(TAG, "Erase [%s] failed and return %d", node->label, err);
+        return OTA_SERV_ERR_REASON_PARTITION_WT_FAIL;
+    }
+    ota_data_partition_write(handle, (char *)&incoming_header, sizeof(flash_tone_header_t));
+    if (need_write_desc) {
+        ota_data_partition_write(handle, (char *)&incoming_desc, sizeof(esp_app_desc_t));
+    }
+    return OTA_SERV_ERR_REASON_SUCCESS;
+}
+
 static esp_err_t ota_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx)
 {
     if (evt->type == OTA_SERV_EVENT_TYPE_RESULT) {
         ota_result_t *result_data = evt->data;
         if (result_data->result != ESP_OK) {
-            ESP_LOGE(TAG, "List id: %d, result: %d, OTA failed", result_data->id, result_data->result);
+            ESP_LOGE(TAG, "List id: %d, OTA failed, result: %d", result_data->id, result_data->result);
         } else {
             ESP_LOGI(TAG, "List id: %d, OTA sucessed", result_data->id);
         }
@@ -92,7 +177,7 @@ void app_ota_start()
             {
                 ESP_PARTITION_TYPE_DATA,
                 "flash_tone",
-                "file://sdcard/flash_tone.bin",
+                "https://github.com/espressif/esp-adf/raw/master/examples/korvo_du1906/tone/audio_tone.bin",
                 NULL
             },
             NULL,
@@ -135,7 +220,11 @@ void app_ota_start()
     for (int i = 0; i < sizeof(upgrade_list) / sizeof(upgrade_list[0]); i++) {
         if (upgrade_list[i].node.type == ESP_PARTITION_TYPE_DATA) {
             ota_data_get_default_proc(&upgrade_list[i]);
-            upgrade_list[i].need_upgrade = ota_sdcard_image_need_upgrade;
+            if (strcmp(upgrade_list[i].node.label, "flash_tone") == 0 && strstr(upgrade_list[i].node.uri, "http")) {
+                upgrade_list[i].need_upgrade = ota_http_tone_image_need_upgrade;
+            } else {
+                upgrade_list[i].need_upgrade = ota_sdcard_image_need_upgrade;
+            }
         } else if (upgrade_list[i].node.type == ESP_PARTITION_TYPE_APP) {
             ota_app_get_default_proc(&upgrade_list[i]);
         } else {
