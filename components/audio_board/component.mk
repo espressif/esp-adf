726721e3a63e30af6cdeb342458d51a539e6946d
higgx <higgx@totollo.com>
2020-11-09 13:24:43 +0800

    add a1s support

--- components/audio_board/component.mk
+++ components/audio_board/component.mk
@@ -41,3 +41,8 @@ ifdef CONFIG_ESP32_S2_KALUGA_1_V1_2_BOARD
 COMPONENT_ADD_INCLUDEDIRS += ./esp32_s2_kaluga_1_v1_2
 COMPONENT_SRCDIRS += ./esp32_s2_kaluga_1_v1_2
 endif
+
+ifdef CONFIG_ESP_AI_THINKER_V2_2_BOARD
+COMPONENT_ADD_INCLUDEDIRS += ./ai_thinker_audio_kit_v2_2
+COMPONENT_SRCDIRS += ./ai_thinker_audio_kit_v2_2
+endif
