200eea2943bcfb6fcd0683b5a5f30c57ca626336
maojianxin <maojianxin@espressif.com>
2021-12-27 14:25:34 +0800

    audio_hal: Add DAC es8156 and ADC es7243e chips

--- components/audio_hal/component.mk
+++ components/audio_hal/component.mk
@@ -20,5 +20,5 @@ COMPONENT_ADD_LDFLAGS += -L$(COMPONENT_PATH)/driver/zl38063/firmware -lfirmware
 COMPONENT_ADD_INCLUDEDIRS += ./driver/tas5805m ./driver/es7148
 COMPONENT_SRCDIRS += ./driver/tas5805m ./driver/es7148
 
-COMPONENT_ADD_INCLUDEDIRS += ./driver/es7210
-COMPONENT_SRCDIRS += ./driver/es7210
+COMPONENT_ADD_INCLUDEDIRS += ./driver/es7210 ./driver/es8156 ./driver/es7243e
+COMPONENT_SRCDIRS += ./driver/es7210 ./driver/es8156 ./driver/es7243e
