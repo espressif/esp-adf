3a6e75eddc33fc19dfc0c357c67dd5fe5b99b651
Qiang Zhang <zhangtemplar@gmail.com>
2022-02-13 22:25:36 -0800

    Create a board for ESP32-A1S-Kit ES8388

--- components/audio_board/component.mk
+++ components/audio_board/component.mk
@@ -51,3 +51,8 @@ ifdef CONFIG_ESP_AI_THINKER_V2_2_BOARD
 COMPONENT_ADD_INCLUDEDIRS += ./ai_thinker_audio_kit_v2_2
 COMPONENT_SRCDIRS += ./ai_thinker_audio_kit_v2_2
 endif
+
+ifdef CONFIG_ESP_AI_THINKER_V2_297_BOARD
+COMPONENT_ADD_INCLUDEDIRS += ./ai_thinker_audio_kit_v2_297
+COMPONENT_SRCDIRS += ./ai_thinker_audio_kit_v2_297
+endif
