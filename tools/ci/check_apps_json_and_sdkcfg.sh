dd73b68b0caffae86c605e6e0a2f7bed77aee9ce
Gao Wei <gaowei@espressif.com>
2022-10-18 20:59:20 +0800

    CI:     1) Fixed the problem that master schdules triggered target_ut_test by mistake.     2) Merge build_ut into build stage.     3) Push operation does not execute the assign_test stage.     4) Add pre_check stage. Pipeline does not succeed unless example_test/unit_test tags are added in MR.     5) Add docs_test pipeline.     6) Add rtmp and av_muxer_sdcard to apps.json.     7) Fix the issue that checking apps.json failed.     8) Add ut_auto_config.sh     9) Delete esp-idf v3.3 related test.

--- tools/ci/check_apps_json_and_sdkcfg.sh
+++ tools/ci/check_apps_json_and_sdkcfg.sh
@@ -6,35 +6,41 @@
 
 [ -z ${IDF_PATH} ] && die "IDF_PATH is not set"
 [ -z ${ADF_PATH} ] && die "ADF_PATH is not set"
+[ -z ${BUILD_PATH} ] && die "BUILD_PATH is not set"
 
-find ${ADF_PATH}"/examples" ! -path "*/managed_components/*" ! -path "*/components/*" -type f -name "Makefile" > examples.txt
-sed -i "s/\/Makefile//g" examples.txt
+set +e
+
+export LIST_JSON_PATH=${BUILD_PATH}/list.json
+
+if ! type jq >/dev/null 2>&1;then
+    apt-get update
+    apt-get install jq -y
+else
+    echo "The jq tool has been installed"
+fi
+
+jq -r .app_dir ${LIST_JSON_PATH} > examples.txt
 cat examples.txt | grep -o '/examples/.*' > check_apps_json_and_sdk.txt
 BOARD_NAME=(`cat $ADF_PATH/tools/ci/apps.json | grep -E "ESP+[0-9a-zA-Z_]+" -o | sort -u -r`)
+APPS_CHECK_RESULT="OK"
 
-check_sdkcfg() {
+function check_sdkcfg() {
     CHIP_TYPES=(`echo $audio_boards | grep -E "esp+[0-9a-z]+" -o | sort -u`)
-
-    if [ ! -e $ADF_PATH${line}"/sdkconfig.defaults" ]; then
-        echo -e "\e[31m Error: sdkconfig.defaults is missing in \e[0m" $line
-        SCRIPT_RESULT="FAIL"
+    if [ ! -f $ADF_PATH${line}"/sdkconfig.defaults" ]; then
+        echo -e "\e[31m ERROR: sdkconfig.defaults is missing in " $line " \e[0m"
+        APPS_CHECK_RESULT="FAIL"
     fi
 
     for chip in ${CHIP_TYPES[*]}
     do
-        if [ ! -e $ADF_PATH${line}"/sdkconfig.defaults."$chip ]; then
-            echo -e "\e[31m Error: sdkconfig.defaults." $chip "is missing in \e[0m" $line
-            SCRIPT_RESULT="FAIL"
+        if [ ! -f $ADF_PATH${line}"/sdkconfig.defaults."$chip ]; then
+            echo -e "\e[31m ERROR: sdkconfig.defaults." $chip "is missing in " $line " \e[0m"
+            APPS_CHECK_RESULT="FAIL"
         fi
     done
-
-    if [ -n "$SCRIPT_RESULT" ]; then
-        echo -e "\033[33m Check sdkconfig.defaults failed \033[0m"
-        exit 1
-    fi
 }
 
-check_audio_board() {
+function check_audio_board() {
     for board_type in ${BOARD_NAME[*]}
     do
         result=$(echo $audio_boards | grep "${board_type}")
@@ -47,12 +53,12 @@ check_audio_board() {
 
     if [ ! -n "$result" ];then
         echo "The error occurs in the definition of" ${line} "in apps.json"
-        echo "\e[31m Error: not found support board \e[0m"
-        exit 1
+        echo -e "\e[31m ERROR: not found support board \e[0m"
+        APPS_CHECK_RESULT="FAIL"
     fi
 }
 
-check_audio_board_idf_ver() {
+function check_audio_board_idf_ver() {
     result=${result%;*}
     chip_board_idf_ver_list=(`echo $result | tr ';' ' '` )
     for chip_board_idf_ver in ${chip_board_idf_ver_list[*]}
@@ -60,24 +66,26 @@ check_audio_board_idf_ver() {
         idf_ver=$(echo $chip_board_idf_ver | grep '\[v[0-9][0-9]' -o)
         if [ ! -n "$idf_ver" ];then
             echo "The error occurs in the definition of" ${line} "in apps.json"
-            echo "\e[31m Error: not found" $chip_board_idf_ver "support idf version \e[0m"
-            exit 1
+            echo -e "\e[31m ERROR: not found" $chip_board_idf_ver "support idf version \e[0m"
+            APPS_CHECK_RESULT="FAIL"
         fi
     done
 }
 
-update_audio_hal() {
-    if [ -e $ADF_PATH${line}"/"$1 ]; then
+function update_audio_hal() {
+    if [ -f $ADF_PATH${line}"/"$1 ]; then
         old_config=$(sed -n '/_BOARD=y/p' $ADF_PATH${line}"/"$1)
         if [ ! -z ${old_config} ]; then
             new_config="CONFIG_"$2"_BOARD=y"
             sed -i "s%${old_config}%${new_config}%" $ADF_PATH${line}"/"$1
-            echo "old config:"$old_config
-            echo "new config:"$new_config
+            # It is easy to see the change of audio_hal
+            # echo $ADF_PATH${line}"/"$1 "AUDIO_HAL:"$old_config " --> " $new_config
         else
             sed -i '$a\CONFIG_'$2'_BOARD=y' $ADF_PATH${line}"/"$1
             cat $ADF_PATH${line}"/"$1
         fi
+    else
+        echo -e "\e[33m WARNING: " $ADF_PATH${line}"/"$1 " skip \e[0m"
     fi
 }
 
@@ -98,9 +106,15 @@ do
         update_audio_hal $1 $2
 
     else
-        echo "\e[31m Error:" $line "exist but not found it in apps.json \e[0m"
-        exit 1
+        echo -e "\e[31m ERROR:" $line "exist but not found it in apps.json \e[0m"
+        APPS_CHECK_RESULT="FAIL"
     fi
 done < check_apps_json_and_sdk.txt
 
-echo "Check passed"
+if [ "$APPS_CHECK_RESULT" = "OK" ];then
+    echo "Check passed"
+elif [ "$APPS_CHECK_RESULT" = "FAIL" ];then
+    echo "Check failure"
+    exit 1
+fi
+set -e
