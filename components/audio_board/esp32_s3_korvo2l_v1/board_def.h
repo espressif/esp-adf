f0d48eaa966b2c1baf8c33bf147327f732789c7f
xutao <xutao@espressif.com>
2022-10-27 15:48:05 +0800

    audio_board: fix undefine  for board korvo2l_v1

--- components/audio_board/esp32_s3_korvo2l_v1/board_def.h
+++ components/audio_board/esp32_s3_korvo2l_v1/board_def.h
@@ -122,6 +122,7 @@
 #define CODEC_ADC_BITS_PER_SAMPLE   I2S_BITS_PER_SAMPLE_16BIT
 #define CODEC_ADC_SAMPLE_RATE       (48000)
 #define RECORD_HARDWARE_AEC         (true)
+#define BOARD_PA_GAIN               (6) /* Power amplifier gain defined by board (dB) */
 
 extern audio_hal_func_t AUDIO_CODEC_ES8311_DEFAULT_HANDLE;
 
