806823bc6cc4823f891d18ebf66785380b2f7550
maojianxin <maojianxin@espressif.com>
2023-02-24 10:45:51 +0800

    audio_stream:Fix bug that raw stream report stop status on timeout

--- components/audio_stream/raw_stream.c
+++ components/audio_stream/raw_stream.c
@@ -50,7 +50,7 @@ int raw_stream_read(audio_element_handle_t pipeline, char *buffer, int len)
     int ret = audio_element_input(pipeline, buffer, len);
     if (ret == AEL_IO_DONE || ret == AEL_IO_OK) {
         audio_element_report_status(pipeline, AEL_STATUS_STATE_FINISHED);
-    } else if (ret < 0) {
+    } else if ((ret < 0) && (ret != AEL_IO_TIMEOUT)) {
         audio_element_report_status(pipeline, AEL_STATUS_STATE_STOPPED);
     }
     return ret;
@@ -61,7 +61,7 @@ int raw_stream_write(audio_element_handle_t pipeline, char *buffer, int len)
     int ret = audio_element_output(pipeline, buffer, len);
     if (ret == AEL_IO_DONE || ret == AEL_IO_OK) {
         audio_element_report_status(pipeline, AEL_STATUS_STATE_FINISHED);
-    } else if (ret < 0) {
+    } else if ((ret < 0) && (ret != AEL_IO_TIMEOUT)) {
         audio_element_report_status(pipeline, AEL_STATUS_STATE_STOPPED);
     }
     return ret;
