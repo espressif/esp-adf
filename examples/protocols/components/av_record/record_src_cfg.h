4b90827a404872ef68e3fa2bd61840e0c1e910e7
tempo <tianhaibo@espressif.com>
2023-05-30 11:36:56 +0800

    esp_rtmp: Use new usb_stream and some enhance
    1: Use latest usb_stream
    2: Set width and height information to server
    3: Remove AAC header when send aac payload

--- examples/protocols/components/av_record/record_src_cfg.h
+++ examples/protocols/components/av_record/record_src_cfg.h
@@ -18,28 +18,14 @@ extern "C" {
 /*
  * Following section configure for USB camera
  */
-#define USB_CAM_PIN_PWDN                        GPIO_NUM_48
-
+#define USB_CAM_PIN_PWDN     CONFIG_USB_CAMERA_POWER_GPIO
+#define USB_CAM_FRAME_BUFFER_SIZE  CONFIG_USB_CAMERA_FRAME_BUFFER_SIZE
 /* USB Camera Descriptors Related MACROS,
 the quick demo skip the standred get descriptors process,
 users need to get params from camera descriptors from PC side,
 eg. run `lsusb -v` in linux,
 then hardcode the related MACROS below
 */
-#define DESCRIPTOR_CONFIGURATION_INDEX          1
-#define DESCRIPTOR_FORMAT_MJPEG_INDEX           2
-#define DESCRIPTOR_FRAME_640_480_INDEX          1
-#define DESCRIPTOR_FRAME_480_320_INDEX          2
-#define DESCRIPTOR_FRAME_352_288_INDEX          3
-#define DESCRIPTOR_FRAME_320_240_INDEX          4
-
-#define DESCRIPTOR_STREAM_INTERFACE_INDEX       1
-#define DESCRIPTOR_STREAM_INTERFACE_ALT_MPS_128 1
-#define DESCRIPTOR_STREAM_INTERFACE_ALT_MPS_256 2
-#define DESCRIPTOR_STREAM_INTERFACE_ALT_MPS_512 3
-#define DESCRIPTOR_STREAM_ISOC_ENDPOINT_ADDR    0x81
-
-#define USB_CAM_ISOC_EP_MPS                     512
 
 #ifdef __cplusplus
 }
