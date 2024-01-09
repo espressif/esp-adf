espressif/esp-adf

commit 5fab75d9cf18c8241f46b4364066995b5f21ef05

audio_recorder: restore ability to select wake word
Commit 5d4ac31913e2 ("audio_recorder: allow setting wake word")
introduced the ability to select which wake word to use at runtime.
This feature was dropped in a later commit. Restore it.

Fixes: 3f6abbb15b85 ("audio_recorder: support new feature of esp-sr (break change of `audio_recorder`)")

components/audio_recorder/recorder_sr.c
@@ -561,21 +561,25 @@ recorder_sr_handle_t recorder_sr_create(recorder_sr_cfg_t *cfg, recorder_sr_ifac
     char *wn_name = NULL;
     char *wn_name_2 = NULL;
 
-    if (recorder_sr->models!=NULL) {
-        for (int i = 0; i< recorder_sr->models->num; i++) {
-            if (strstr(recorder_sr->models->model_name[i], ESP_WN_PREFIX) != NULL) {
-                if (wn_name == NULL) {
-                    wn_name = recorder_sr->models->model_name[i];
-                    ESP_LOGI(TAG, "The first wakenet model: %s\n", wn_name);
-                } else if (wn_name_2 == NULL) {
-                    wn_name_2 = recorder_sr->models->model_name[i];
-                    ESP_LOGI(TAG, "The second wakenet model: %s\n", wn_name_2);
+    if (recorder_sr->wn_wakeword == NULL) {
+        if (recorder_sr->models!=NULL) {
+            for (int i = 0; i< recorder_sr->models->num; i++) {
+                if (strstr(recorder_sr->models->model_name[i], ESP_WN_PREFIX) != NULL) {
+                    if (wn_name == NULL) {
+                        wn_name = recorder_sr->models->model_name[i];
+                        ESP_LOGI(TAG, "The first wakenet model: %s\n", wn_name);
+                    } else if (wn_name_2 == NULL) {
+                        wn_name_2 = recorder_sr->models->model_name[i];
+                        ESP_LOGI(TAG, "The second wakenet model: %s\n", wn_name_2);
+                    }
                 }
             }
+        } else {
+            ESP_LOGE(TAG, "Please enable wakenet model and select wake word by menuconfig!\n");
+            goto _failed;
         }
     } else {
-        ESP_LOGE(TAG, "Please enable wakenet model and select wake word by menuconfig!\n");
-        goto _failed;
+        wn_name = esp_srmodel_filter(recorder_sr->models, ESP_WN_PREFIX, recorder_sr->wn_wakeword);
     }
 
     cfg->afe_cfg.wakenet_model_name = wn_name;

commit 57de02c0151e86a50087ffd96fc3e0d1f263ffe2

recorder_sr: fix crash if WakeNet model is missing
When the SR recorder is created with specific wake word, and that wake
word is missing from the model partition, the following crash occurs:

E (5703) AFE_SR: ERROR: Please select wake words!

Guru Meditation Error: Core  0 panic'ed (LoadProhibited). Exception was unhandled.

Core  0 register dump:
PC      : 0x420703ab  PS      : 0x00060030  A0      : 0x82015d72  A1      : 0x3fcb9e80
A2      : 0x3c26720c  A3      : 0x3fcb9ef0  A4      : 0x00000000  A5      : 0x00000000
A6      : 0x00000001  A7      : 0x3c266f9c  A8      : 0x8207034c  A9      : 0x3fcb9e20
A10     : 0x00000000  A11     : 0x3c1c6e0c  A12     : 0x3c1c7680  A13     : 0x00001647
A14     : 0x3c1c6e0c  A15     : 0x447fc000  SAR     : 0x00000004  EXCCAUSE: 0x0000001c
EXCVADDR: 0x00000008  LBEG    : 0x400556d5  LEND    : 0x400556e5  LCOUNT  : 0xfffffffe

Backtrace: 0x420703a8:0x3fcb9e80 0x42015d6f:0x3fcb9eb0 0x4200cb32:0x3fcb9ef0 0x4200ce2b:0x3fcba0e0 0x4200b785:0x3fcba1b0 0x42170f06:0x3fcba260

Fix this by adding a NULL check.

Fixes: 5fab75d9cf18 ("audio_recorder: restore ability to select wake word")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

components/audio_recorder/recorder_sr.c
@@ -580,6 +580,10 @@ recorder_sr_handle_t recorder_sr_create(recorder_sr_cfg_t *cfg, recorder_sr_ifac
         }
     } else {
         wn_name = esp_srmodel_filter(recorder_sr->models, ESP_WN_PREFIX, recorder_sr->wn_wakeword);
+        if (wn_name == NULL) {
+            ESP_LOGE(TAG, "Please enable wakenet model and select wake word (%s) by menuconfig!", recorder_sr->wn_wakeword);
+            goto _failed;
+        }
     }
 
     cfg->afe_cfg.wakenet_model_name = wn_name;