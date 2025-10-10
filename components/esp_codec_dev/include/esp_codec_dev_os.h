78c5e6efeb2fe8e57924810fa9e2c089e7ace69f
tempo <tianhaibo@espressif.com>
2025-10-10 15:20:48 +0800

    bugfix(esp_codec_dev): Fixed racing condition when playback record re-enable during another running

--- components/esp_codec_dev/include/esp_codec_dev_os.h
+++ components/esp_codec_dev/include/esp_codec_dev_os.h
@@ -11,8 +11,53 @@ extern "C" {
 #endif
 
 /**
- * @brief         Sleep in milliseconds
- * @param         ms: Sleep time (unit ms)
+ * @brief  Codec device mutex handle
+ */
+typedef void *esp_codec_dev_mutex_handle_t;
+
+/**
+ * @brief  Create mutex for codec device
+ *
+ * @return
+ *       - NULL    No available resource
+ *       - Others  Mutex handle
+ */
+esp_codec_dev_mutex_handle_t esp_codec_dev_mutex_create(void);
+
+/**
+ * @brief  Lock mutex for codec device
+ *
+ * @param[in]  mutex       Mutex handle
+ * @param[in]  timeout_ms  Wait timeout for mutex (unit ms)
+ *
+ * @return
+ *       - 0       On success
+ *       - Others  Failed to lock
+ */
+int esp_codec_dev_mutex_lock(esp_codec_dev_mutex_handle_t mutex, int timeout_ms);
+
+/**
+ * @brief  Unlock mutex for codec device
+ *
+ * @param[in]  mutex  Mutex handle
+ *
+ * @return
+ *       - 0       On success
+ *       - Others  Failed to unlock
+ */
+int esp_codec_dev_mutex_unlock(esp_codec_dev_mutex_handle_t mutex);
+
+/**
+ * @brief  Destroy mutex for codec device
+ *
+ * @param[in]  mutex  Mutex handle
+ */
+void esp_codec_dev_mutex_destroy(esp_codec_dev_mutex_handle_t mutex);
+
+/**
+ * @brief  Sleep in milliseconds
+ *
+ * @param[in]  ms  Sleep time (unit ms)
  */
 void esp_codec_dev_sleep(int ms);
 
